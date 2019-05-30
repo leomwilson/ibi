47b19ad46b9ad67b11905dc80ea8ff9bef1989ec
Leo Wilson <leowilson.xlw123@gmail.com>
2019-05-30 16:11:49 -0700

    Add PWD detection

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 use colored::*;
+use std::env;
+
+fn pwd() -> String {
+    let home = env::var("HOME").unwrap(); // get the user's home directory
+    let path = format!("{}", env::current_dir().unwrap_or("".into()).display()); // get the current path
+    if path.starts_with(&home) {
+        path.replace(&home, "~")
+    }
+    else {
+        path
+    }
+}
 
 fn main() {
-    println!("{}", "Hello, world!".bright_blue());
+    println!("{}", pwd().bright_blue());
 }
