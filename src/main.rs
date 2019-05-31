993e1e54da66cbaf9e2bcc6c7092363e942c1c29
Leo Wilson <leowilson.xlw123@gmail.com>
2019-05-30 17:09:19 -0700

    Add a prompt character

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,38 @@ use colored::*;
 use std::env;
 use std::path::Path;
 
-fn pwd() -> String {
+fn pwd() -> colored::ColoredString {
     let home = env::var("HOME").unwrap(); // get the user's home directory
-    let path = format!("{}", env::current_dir().unwrap_or("".into()).display()); // get the current path
+    let mut path = format!("{}", env::current_dir().unwrap_or("".into()).display()); // get the current path
     if path.starts_with(&home) {
-        path.replacen(&home, "~", 1) // only replace the first instance
+        path = path.replacen(&home, "~", 1); // only replace the first instance
     }
-    else {
-        path
-    }
+    return path.cyan()
 }
 
-fn venv() -> String {
+fn venv() -> colored::ColoredString {
     match env::var("VIRTUAL_ENV") {
         Ok(path) => {
             if let Some(name) = Path::new(&path).file_name() {
                 if let Some(utf8_name) = name.to_str() { // .file_name() returns an OsString, we want a proper String (UTF-8 only)
-                    return format!("[{}] ", utf8_name);
+                    return format!("[{}] ", utf8_name).yellow().dimmed();
                 }
             }
         },
         Err(_) => (),
     }
-    return "".to_string();
+    return "".white();
+}
+
+fn pchar() -> colored::ColoredString {
+    if env::var("HOME").unwrap() == "/root" {
+        "#".red().bold()
+    }
+    else {
+        "$".bright_magenta()
+    }
 }
 
 fn main() {
-    println!("{} {}", pwd().bright_blue(), venv().yellow().dimmed());
+    println!("{} {}\n{} ", pwd(), venv(), pchar());
 }
